6a94d03a61f09fda51ce142c8cf05541788605ad
Markus Klinga <mail@mklinga.com>
2021-11-27 11:08:34 +0100

    more complete mock data

--- src/journal.js
+++ src/journal.js
@@ -2,7 +2,18 @@ const express = require("express");
 const journal = express();
 
 const loadJournal = date => {
-    return { mood: '😍', title: 'First journal / title', journal: 'This might be the best there ever is or was or will be.' }
+    return {
+        mood: '😍',
+        title: 'First journal / title',
+        journal: 'This might be the best there ever is or was or will be.',
+        social: [
+            { completed: true, description: 'Social 1' },
+            { completed: true, description: 'Social 2' }
+        ],
+        physical: [
+            { completed: true, description: 'Physical 1' }
+        ]
+    }
 }
 
 journal.get('/:date', (req, res) => {
